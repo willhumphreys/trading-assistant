cbc70a1ab8c99cf1ddb603d89d0678a27809fcd1
will <whumphreys@gmail.com>
2023-12-03 22:22:28 +0100

    Set the windows-startup scripts

--- backend/scripts/start-server.py
+++ backend/scripts/start-server.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import subprocess
 import sys
 
@@ -18,12 +19,21 @@ profile_name: str = sys.argv[1]
 
 print(f"Starting server with profile: {profile_name}")
 
-# Define the path to your JDK and the jar file
-java_home = "/home/will/.jdks/temurin-17.0.9"
-jar_path = "target/darwinex-client-0.0.1-SNAPSHOT.jar"
+# Define the JDK path based on the OS
+if platform.system() == 'Windows':
+    java_home = r"C:\Users\user\.jdks\temurin-17.0.9"
+else:
+    java_home = "/home/will/.jdks/temurin-17.0.9"
+
+# Define the path to the jar file
+jar_path = "darwinex-client-0.0.1-SNAPSHOT.jar"
 
 # Set JAVA_HOME for this session
 os.environ["JAVA_HOME"] = java_home
 
+# Set the executable based on the OS
+java_executable = 'java.exe' if platform.system() == 'Windows' else 'java'
+
 # Start the server with the provided profile name
-subprocess.run([os.path.join(java_home, 'bin', 'java'), '-jar', jar_path, f'--spring.profiles.active={profile_name}'])
+subprocess.run(
+    [os.path.join(java_home, 'bin', java_executable), '-jar', jar_path, f'--spring.profiles.active={profile_name}'])
---
+++ backend/scripts/start-windows-both.sh
@@ -0,0 +1,1 @@
+python start-server.py windows-both
