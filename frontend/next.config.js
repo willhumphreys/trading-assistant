bba5e55299454b008cbdfca7cddeb02c52e2a915
will <whumphreys@gmail.com>
2023-12-24 19:51:30 +0100

    Try the frontend build again

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     async rewrites() {
         const backendHost = process.env.NEXT_PUBLIC_BACKEND_HOST || 'http://trading-assistant-service:8080';
         return [
