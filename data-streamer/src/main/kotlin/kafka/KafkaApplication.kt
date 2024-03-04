abc0fb63c105e8457007728d8bb67efaab575a2e
will <whumphreys@gmail.com>
2024-03-04 15:32:24 +0100

    Only send update if the file has changed.

--- data-streamer/src/main/kotlin/kafka/KafkaApplication.kt
+++ data-streamer/src/main/kotlin/kafka/KafkaApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.ConfigurableApplicationContext
 
 @SpringBootApplication
-class KafkaApplication
+open class KafkaApplication
 
 fun main(args: Array<String>) {
     val context: ConfigurableApplicationContext = SpringApplication.run(KafkaApplication::class.java, *args)
--- data-streamer/src/main/kotlin/kafka/service/OrderFileWatcherService.kt
+++ data-streamer/src/main/kotlin/kafka/service/OrderFileWatcherService.kt
@@ -14,83 +14,109 @@ import org.springframework.stereotype.*
 import java.io.*
 import java.nio.charset.*
 import java.nio.file.*
+import java.security.*
 
 @Service
-class OrderFileWatcherService(
+open class OrderFileWatcherService(
     private val kafkaTemplate: KafkaTemplate<String, String>,
     @param:Value("\${metatrader-directory}") private val directory: String,
     @param:Value("\${orders-topic}") private val ordersTopic: String
 ) {
-
-    private val logger = LogManager.getLogger(OrderFileWatcherService::class.java)
-    private val objectMapper = ObjectMapper().apply {
-        registerKotlinModule()
-        registerModule(JavaTimeModule())
-    }
+    private val logger: Logger = LogManager.getLogger(
+        OrderFileWatcherService::class.java
+    )
+    private val objectMapper: ObjectMapper =
+        ObjectMapper().registerModule(KotlinModule()).registerModule(JavaTimeModule())
+    private val fileHashes: MutableMap<String, String> = HashMap()
 
     @EventListener(ApplicationStartedEvent::class)
     @Async
-    fun watchFileChanges() {
-        val watchService = FileSystems.getDefault().newWatchService()
-        val path = Paths.get(directory)
-        path.register(
-            watchService,
-            StandardWatchEventKinds.ENTRY_CREATE,
-            StandardWatchEventKinds.ENTRY_DELETE,
-            StandardWatchEventKinds.ENTRY_MODIFY
-        )
+    open fun watchFileChanges() {
+        try {
+            FileSystems.getDefault().newWatchService().use { watchService ->
+                val path = Paths.get(directory)
+                path.register(
+                    watchService,
+                    StandardWatchEventKinds.ENTRY_CREATE,
+                    StandardWatchEventKinds.ENTRY_DELETE,
+                    StandardWatchEventKinds.ENTRY_MODIFY
+                )
+                logger.info("Monitoring directory for changes...")
+                while (!Thread.currentThread().isInterrupted) {
+                    val key = watchService.take()
+                    for (event in key.pollEvents()) {
+                        val kind = event.kind()
 
-        logger.info("Monitoring directory for changes...")
+                        if (kind === StandardWatchEventKinds.ENTRY_MODIFY) {
+                            val ev = event as WatchEvent<Path>
+                            val fileName = ev.context().toString()
 
-        while (true) {
-            val key: WatchKey
-            try {
-                key = watchService.take()
-            } catch (ex: InterruptedException) {
-                Thread.currentThread().interrupt()
-                logger.error("File monitoring interrupted.", ex)
-                return
-            }
+                            if ("DWX_Orders.json" == fileName) {
+                                val filePath = path.resolve(fileName)
+                                val currentHash = getFileContentHash(filePath)
 
-            for (event in key.pollEvents()) {
-                val kind = event.kind()
-
-                // Check for modify events and read the file's contents
-                if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
-
-
-                    @Suppress("UNCHECKED_CAST")
-                    val ev = event as WatchEvent<Path>
-                    val fileName = ev.context().toString()
-
-                    logger.info("Event kind: $kind. File affected: $fileName.")
-                    try {
-                        if (fileName == "DWX_Orders.json") {
-                            val filePath = path.resolve(fileName)
-                            try {
-                                Files.newBufferedReader(filePath, StandardCharsets.UTF_8).use { reader ->
-                                    val orders = objectMapper.readValue(reader, Orders::class.java)
-                                    logger.info("Deserialized orders: $orders")
-
-                                    val ordersJson = objectMapper.writeValueAsString(orders)
-
-                                    kafkaTemplate.send(ordersTopic, ordersJson)
-                                    logger.info("Deserialized orders sent to Kafka topic: $orders")
+                                if (currentHash != fileHashes[fileName]) {
+                                    fileHashes[fileName] = currentHash
+                                    processFile(filePath, fileName)
                                 }
-                            } catch (e: IOException) {
-                                logger.error("Error reading file: $fileName", e)
                             }
                         }
-                    } catch (e: IOException) {
-                        logger.error("Error reading file: $fileName", e)
+                    }
+                    val valid = key.reset()
+                    if (!valid) {
+                        break
                     }
                 }
             }
+        } catch (e: IOException) {
+            Thread.currentThread().interrupt()
+            logger.error("File monitoring interrupted.", e)
+        } catch (e: InterruptedException) {
+            Thread.currentThread().interrupt()
+            logger.error("File monitoring interrupted.", e)
+        }
+    }
 
-            val valid = key.reset()
-            if (!valid) {
-                break
+    @Throws(IOException::class)
+    private fun getFileContentHash(filePath: Path): String {
+        try {
+            val digest = MessageDigest.getInstance("SHA-256")
+            val fileContent = Files.readAllBytes(filePath)
+            val encodedHash = digest.digest(fileContent)
+            return bytesToHex(encodedHash)
+        } catch (e: NoSuchAlgorithmException) {
+            logger.error("Algorithm for hashing not found.", e)
+            return ""
+        }
+    }
+
+    private fun processFile(filePath: Path, fileName: String) {
+        try {
+            Files.newBufferedReader(filePath, StandardCharsets.UTF_8).use { reader ->
+                val orders =
+                    objectMapper.readValue(reader, Orders::class.java)
+                logger.info("Deserialized orders: $orders")
+
+                val ordersJson = objectMapper.writeValueAsString(orders)
+                kafkaTemplate.send(ordersTopic, ordersJson)
+                logger.info("Deserialized orders sent to Kafka topic: $orders")
             }
+        } catch (e: IOException) {
+            logger.error("Error reading file: $fileName", e)
+        }
+    }
+
+    companion object {
+        private fun bytesToHex(hash: ByteArray): String {
+            val hexString = StringBuilder(2 * hash.size)
+            for (b in hash) {
+                val hex = Integer.toHexString(0xff and b.toInt())
+                if (hex.length == 1) {
+                    hexString.append('0')
+                }
+                hexString.append(hex)
+            }
+            return hexString.toString()
         }
     }
 }
