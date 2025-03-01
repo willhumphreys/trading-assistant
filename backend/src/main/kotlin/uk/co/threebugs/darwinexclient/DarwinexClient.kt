566b3b23099a9b2f6b161f057424da11835d9687
will <whumphreys@gmail.com>
2025-03-01 11:34:17 +0100

    Enable JPA Auditing in DarwinexClientApplication
    Added the @EnableJpaAuditing annotation to enable auditing features for JPA entities. This change is necessary to automatically manage audit-related fields like created and modified timestamps.

--- backend/src/main/kotlin/uk/co/threebugs/darwinexclient/DarwinexClient.kt
+++ backend/src/main/kotlin/uk/co/threebugs/darwinexclient/DarwinexClient.kt
@@ -2,10 +2,12 @@ package uk.co.threebugs.darwinexclient
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing
 class DarwinexClientApplication
 
 fun main(args: Array<String>) {
